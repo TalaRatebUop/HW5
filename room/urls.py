605c9166a4a0af03dc8a08396974d8f240c2a999
Home <202110068@uopstd.edu.jo>
2023-11-28 22:19:25 +0300

    update 1:create project  reservation and room update2:create a url update3:create form

--- room/urls.py
+++ room/urls.py
@@ -2,5 +2,7 @@ from  django.urls import path
 from . import  views
 urlpatterns=[
     path("",views.index2),
-    path('add',views.add, name="add")
+    path('add',views.add, name="add"),
+    path("product1",views.product1,name="product1"),
+    path('addP',views.addProduct,name="addP")
              ]
--- room/views.py
+++ room/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
-
+from . import Form
 # Create your views here.
 RoomNumber=[2233,4566,222,56,22224]
 def index2(request):
     return render(request,"info.html",{"RoomNumber":RoomNumber})
 def add(request):
     return render(request,"add.html")
+def product1(request):
+    return render(request,"product.html",{"product":product})
+product=[]
+def addProduct(request):
+     if request.method=="POST":
+         p = Form.product(request.POST)
+
+         if p.is_valid():
+            product.append(request.POST)
+            return render(request,"product.html")
+
+     else :
+         p2=Form.product(request.POST)
+         return render(request,"addProduct.html",{"form":p2})

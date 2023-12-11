6f7ee52f835cf88ef0f7931502563c124f8ed689
Home <202110068@uopstd.edu.jo>
2023-12-11 17:11:16 +0300

    update 1:create project  reservation and room update2:create a url update3:create django form HW6 update4:Hw7 session update5:create super user

---
+++ reservation/room/urls.py
@@ -0,0 +1,8 @@
+from  django.urls import path
+from . import  views
+urlpatterns=[
+    path("", views.index2),
+    path('add', views.add, name="add"),
+    path('add2', views.add2, name="add2"),
+    path('info2', views.info2, name="info2"),
+             ]
--- room/urls.py
+++
@@ -1,8 +0,0 @@
-from  django.urls import path
-from . import  views
-urlpatterns=[
-    path("",views.index2),
-    path('add',views.add, name="add"),
-    path('add2',views.add2, name="add2"),
-    path('info2',views.info2, name="info2"),
-             ]

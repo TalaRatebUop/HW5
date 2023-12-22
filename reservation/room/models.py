c17fa018ee7888c3a767f466d849814809b88ccf
Home <202110068@uopstd.edu.jo>
2023-12-22 22:24:40 +0300

    update 1:create project  reservation and room update2:create a url update3:create django form HW6 update4:Hw7 session update5:create super user

--- reservation/room/models.py
+++ reservation/room/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class ClientType(models.Model):
+    type=models.IntegerField(primary_key=True,default=0)
+    name=models.CharField(max_length=20)
+    description=models.CharField(null=False,max_length=25)
+    def __str__(self):
+        return self.name
+
+class Client(models.Model):
+    cid=models.IntegerField(primary_key=True,default=0)
+    name=models.CharField(max_length=20)
+    gender_ch=[(1,"Female"),(2,"Male")]
+    gender=models.IntegerField(choices=gender_ch)
+    email=models.EmailField(null=True, unique=True)
+    birthdate=models.DateField()
+    type=models.ForeignKey(ClientType,on_delete=models.CASCADE,default=0)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    Pid=models.IntegerField(primary_key=True,default=0)
+    name=models.CharField(max_length=20)
+    description=models.CharField(null=False,max_length=20)
+    def __str__(self):
+        return self.name
+class Order(models.Model):
+    Oid=models.IntegerField(primary_key=True,default=0)
+    Pid=models.ForeignKey(Product,on_delete=models.CASCADE,default=0)
+    cid = models.ForeignKey(Client,on_delete=models.CASCADE,default=0)
+    OrderDate = models.DateField()
+    ShippingDate = models.DateField()
+    def __int__(self):
+        return self.Oid
